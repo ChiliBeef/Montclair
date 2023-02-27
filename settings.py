66c43edc2d2e8ecfd850dbb95ee4c9d81cb5d8bd
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 10:50:41 -0500

    Settings
    Contains settings for Display, Colors, and Font setup.

---
+++ settings.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+### Display Setup ###
+pygame.display.set_caption("Montclair")
+WINDOW = pygame.display.set_mode((1000, 600))
+FPS = 60
+clock = pygame.time.Clock()
+
+
+### Colors ###
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
+
+
+### Font Setup ###
+dialogue_font = pygame.font.SysFont("helvetica", 20)
+option_font = pygame.font.SysFont("helvetica", 20)
