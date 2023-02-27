17dd5a8a0e7af00e003de54b9dc6f37d288b0228
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 10:51:21 -0500

    Update main.py
    Corrected code to reflect calling upon settings.py.

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import pygame
-
+from settings import *
 
 pygame.init()
 
-
-### Display Setup ###
-pygame.display.set_caption("Montclair")
-WINDOW = pygame.display.set_mode((1000, 600))
-FPS = 60
-clock = pygame.time.Clock()
-
-
-### Colors ###
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (255, 255, 0)
-
-
-### Font Setup ###
-dialogue_font = pygame.font.SysFont("helvetica", 20)
-option_font = pygame.font.SysFont("helvetica", 20)
 
 
 ### First dialogue ###
