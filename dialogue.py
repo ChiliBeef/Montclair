7d23747a60420b7568cff53e66275f2c26b59c2f
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 11:22:58 -0500

    Dialogue.py
    Contains setup of font for dialogue and options text in game.

---
+++ dialogue.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+
+
+class Dialogue:
+    def __init__(self):
+        self.font = pygame.font.SysFont("helvetica", 20)
+        self.color = WHITE
+
+class Options:
+    def __init__(self):
+        self.font = option_font = pygame.font.SysFont("helvetica", 20)
+        self.color = YELLOW
