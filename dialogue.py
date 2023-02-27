02fc3b2abd889c2b9f2f31e978f0ebea2ada7bd3
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 12:13:16 -0500

    Update dialogue.py

--- dialogue.py
+++ dialogue.py
@@ -3,12 +3,18 @@ from settings import *
 
 
 
-class Dialogue:
+class Dialogue(pygame.surface.Surface):
     def __init__(self):
         self.font = pygame.font.SysFont("helvetica", 20)
         self.color = WHITE
+    
+    def render(self, text):
+        self.font.render(f"{text}", True, self.color)
 
-class Options:
+class Options(pygame.surface.Surface):
     def __init__(self):
         self.font = pygame.font.SysFont("helvetica", 20)
         self.color = YELLOW
+
+    def render(self, text):
+        self.font.render(f"{text}", True, self.color)
