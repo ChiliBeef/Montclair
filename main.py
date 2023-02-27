1c72469a65a40203e99ad38939f3948eb938da99
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 13:35:42 -0500

    Main.py
    Changed code for displaying dialogue and options.

--- main.py
+++ main.py
@@ -8,22 +8,15 @@ pygame.init()
 
 ### First dialogue ###
 dialogue = Dialogue()
-dialogue.render("Here we have some standard text and dialogue.")
-WINDOW.blit(dialogue, (20, 10))
-dialogue.render("Here is even more standard text and dialogue.")
-WINDOW.blit(dialogue, (20, 40))
-
+dialogue.render("Here we have some standard text and dialogue.", 20, 10)
+dialogue.render("Here we have some standard text and dialogue.", 20, 30)
 
 ### First set of choices ###
-option1, option2 = Options()
+option1 = Options()
+option2 = Options()
 
-option1.render("This is Option 1.")
-WINDOW.blit(option1, (20, 80))
-option1_rect = pygame.Rect(20, 80, 145, 20)
-
-option2.render("This is Option 2.")
-WINDOW.blit(option2, (20, 100))
-option2_rect = pygame.Rect(20, 100, 145, 20)
+option1.render("This is Option 1.", 20, 80)
+option2.render("This is Option 2.", 20, 100)
 
 
 ### Game loop ###
@@ -38,21 +31,19 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
                 
-                if option1_rect.collidepoint(mouse_pos):
+                if option1.rect.collidepoint(mouse_pos):
                     WINDOW.fill(BLACK)
-                    option1_text = option1.render("Welcome to Option 1")
-                    WINDOW.blit(option1_text, (300, 300))
+                    option1_text = option1.render("Welcome to Option 1", 300, 300)
                     print("You clicked on Option 1.")
-                    option1_rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
-                    option2_rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
+                    option1.rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
+                    option2.rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
 
-                elif option2_rect.collidepoint(mouse_pos):
+                elif option2.rect.collidepoint(mouse_pos):
                     WINDOW.fill(BLACK)
-                    option2_text = option2.render("Welcome to Option 2")
-                    WINDOW.blit(option2_text, (300, 300))
+                    option2_text = option2.render("Welcome to Option 2", 300, 300)
                     print("You clicked on Option 2.")
-                    option1_rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
-                    option2_rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
+                    option1.rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
+                    option2.rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
 
 
     pygame.display.flip()
