63b8f1cbede8f7d9a13976e8107396e0bc8ed30e
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-27 13:36:54 -0500

    dialogue.py
    Changed code to display dialogue and options better in main.py.

--- dialogue.py
+++ dialogue.py
@@ -3,18 +3,29 @@ from settings import *
 
 
 
-class Dialogue(pygame.surface.Surface):
+class Dialogue():
     def __init__(self):
         self.font = pygame.font.SysFont("helvetica", 20)
         self.color = WHITE
-    
-    def render(self, text):
-        self.font.render(f"{text}", True, self.color)
+        self.displaySurface = pygame.display.get_surface()
 
-class Options(pygame.surface.Surface):
+
+    def render(self, text, x, y):
+        self.position = (x, y)
+        self.rect = pygame.rect.Rect(self.position[0], self.position[1], 145, 20)
+        dialogue = self.font.render(f"{text}", True, self.color)
+        dialoguePosition = dialogue.get_rect(x=self.position[0], y=self.position[1])   # Position the dialogue
+        self.displaySurface.blit(dialogue, dialoguePosition)
+
+class Options():
     def __init__(self):
         self.font = pygame.font.SysFont("helvetica", 20)
         self.color = YELLOW
-
-    def render(self, text):
-        self.font.render(f"{text}", True, self.color)
+        self.displaySurface = pygame.display.get_surface()
+        
+    def render(self, text, x, y):
+        self.position = (x, y)
+        self.rect = pygame.rect.Rect(self.position[0], self.position[1], 145, 20)
+        dialogue = self.font.render(f"{text}", True, self.color)
+        dialoguePosition = dialogue.get_rect(x=self.position[0], y=self.position[1])   # Position the dialogue
+        self.displaySurface.blit(dialogue, dialoguePosition)
