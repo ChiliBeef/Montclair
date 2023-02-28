5b0afa3b76559a3fbca0b080938a049786c4737d
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-28 17:39:54 -0500

    main.py
    Added import of page_1.py.
    Cleaned up code.
    Created a Title Screen and set its font settings in font_settings.py.

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
 import pygame
 from settings import *
-from dialogue import *
+from font_settings import *
+from page_1 import page_1
 
 pygame.init()
 
 
 
-### First dialogue ###
-dialogue = Dialogue()
-dialogue.render("Here we have some standard text and dialogue.", 20, 10)
-dialogue.render("Here we have some standard text and dialogue.", 20, 30)
-
-### First set of choices ###
-option1 = Options()
-option2 = Options()
-
-option1.render("This is Option 1.", 20, 80)
-option2.render("This is Option 2.", 20, 100)
+### Title Screen ###
+title, title_rect = title_font()
+WINDOW.blit(title, title_rect)
 
 
 ### Game loop ###
-run = True
-while run:
-    clock.tick(FPS)
-
+running = True
+while running:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            run = False
+            running = False
     
         if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                
-                if option1.rect.collidepoint(mouse_pos):
-                    WINDOW.fill(BLACK)
-                    option1_text = option1.render("Welcome to Option 1", 300, 300)
-                    print("You clicked on Option 1.")
-                    option1.rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
-                    option2.rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
-
-                elif option2.rect.collidepoint(mouse_pos):
-                    WINDOW.fill(BLACK)
-                    option2_text = option2.render("Welcome to Option 2", 300, 300)
-                    print("You clicked on Option 2.")
-                    option1.rect.topleft = (-100, -100)  # Remove clickable area from Option 1 by putting it off screen.
-                    option2.rect.topleft = (-100, -100)  # Remove clickable area from Option 2 by putting it off screen.
+            if title_rect.collidepoint(event.pos):
+                    page_1(WINDOW)
+                    title_rect.topleft = (-100, -100)  # Remove clickable area from Title by putting it off screen.
 
 
-    pygame.display.flip()
+    pygame.display.update()
