fd2291297f6a2f6c15e52b707374feb91446ab29
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-02-28 14:06:40 -0500

    settings.py
    Updated display settings to a more reasonable 1280 x 720.

--- settings.py
+++ settings.py
@@ -1,12 +1,14 @@
 import pygame
 
 
+
 ### Display Setup ###
 pygame.display.set_caption("Montclair")
-WINDOW = pygame.display.set_mode((1000, 600))
+WIDTH = 1280
+HEIGHT = 720
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 clock = pygame.time.Clock()
-
 
 ### Colors ###
 WHITE = (255, 255, 255)
