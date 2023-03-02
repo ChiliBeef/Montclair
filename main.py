28af4f18f7e1adfb8a950077095267813f59c455
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-03-02 14:19:07 -0500

    main.py
    Updated/changed some of the code.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+import sys
 import pygame
 from settings import *
 from font_settings import *
-from page_1 import page_1
+from office_0 import office_0
 
 pygame.init()
 
 
 
 ### Title Screen ###
-title, title_rect = title_font()
+title, title_rect = title_text()
 WINDOW.blit(title, title_rect)
 
 
@@ -19,11 +20,14 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
-    
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if title_rect.collidepoint(event.pos):
-                    page_1(WINDOW)
-                    title_rect.topleft = (-100, -100)  # Remove clickable area from Title by putting it off screen.
+                office_0(WINDOW)                    # This will begin the game by taking you to your office.
+                title_rect.topleft = (-100, -100)   # Remove clickable area from Title by putting it off screen.
 
 
     pygame.display.update()
+
+pygame.quit()
+sys.exit()
