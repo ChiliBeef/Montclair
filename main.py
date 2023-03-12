26846c163e0e66834b16b965c61bf084eb6bfced
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-03-12 12:28:46 -0400

    Updated main.py
    Added in code to play a glitch sound effect to coincide with the glitch video effect.

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ title, title_rect = title_text()
 WINDOW.blit(title, title_rect)
 
 
+# Load title screen background image and glitch vfx files to be played.
 glitch1 = pygame.image.load("assets/computer/server_cabinets_glitch1.png")
 glitch2 = pygame.image.load("assets/computer/server_cabinets_glitch2.png")
 
+# Load title screen background sound and glitch sfx file to be played.
+bg_sound = pygame.mixer.Sound("assets/sounds/title_screen.wav")
+glitch_sound = pygame.mixer.Sound("assets/sounds/glitch_sfx.wav")
+bg_sound.play(-1)
 
+# Set up animation sequence of background image and glitch vfx.
 current_frame_index = 0
-animation_frames = {0 : [glitch1, 2000], 1 : [glitch2, 100] , 2 : [glitch1, 50] , 3 :  [glitch2, 100] }
+animation_frames = {0 : [glitch1, 2000, None], 1 : [glitch2, 100, glitch_sound] , 2 : [glitch1, 1500, None] , 3 :  [glitch2, 100, glitch_sound] }
 current_frame = animation_frames[current_frame_index][0]
 frame_time = animation_frames[current_frame_index][1]
 first_time = time.time()
@@ -25,9 +31,7 @@ flip_frame = pygame.event.custom_type()
 pygame.time.set_timer(flip_frame, frame_time)
 
 
-# Load title screen sound file to be played.
-sound_effect = pygame.mixer.Sound("assets/sounds/title_screen.wav")
-sound_effect.play(-1)
+
 
 ### Game loop ###
 running = True
@@ -49,7 +53,14 @@ while running:
             current_frame = animation_frames[current_frame_index][0]
             frame_time = animation_frames[current_frame_index][1]
             first_time = time.time()
+
+            if animation_frames[current_frame_index][2] is not None:
+                sound_2_play = animation_frames[current_frame_index][2]
+                sound_2_play.play()
+                print("change")
+
             pygame.time.set_timer(flip_frame, frame_time)
+
 
     second_time = time.time()
     elapsed_time = (second_time - first_time) * 1000
