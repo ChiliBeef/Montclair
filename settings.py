0b6bea3ccb4a641a38d0bb0cdf77fff461a11533
ChiliBeef <97184530+ChiliBeef@users.noreply.github.com>
2023-03-12 11:37:24 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 import pygame
 
 
-
 ### Display Setup ###
 pygame.display.set_caption("Montclair")
 WIDTH = 1280
@@ -9,9 +8,11 @@ HEIGHT = 720
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 clock = pygame.time.Clock()
+clock.tick(FPS)
 
 
 ### Colors ###
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
